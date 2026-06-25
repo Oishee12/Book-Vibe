d3303ba7bf6bb5001ac9497f31e5d7893d4eca3f
Oishe <yhsiokhan@gmail.com>
2026-06-25 12:36:56 +0600

    Create routes component and move code from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router'
-import MainLayout from './layout/MainLayout';
-import HomePage from './pages/homepage/HomePage';
-import Books from './pages/Books';
+import { RouterProvider } from 'react-router'
+import { router } from './routes/Routes'
 
-const router = createBrowserRouter([
- {
-  path: "/",
-  Component: MainLayout,
-  children:
-  [
-    {
-      index: true,
-      element: <HomePage></HomePage>
-    }, 
-    {
-      path: "/books",
-      element: <Books></Books>
-    }
-  ]
- }
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import MainLayout from '../layout/MainLayout';
+import HomePage from '../pages/homepage/HomePage';
+import Books from '../pages/Books';
+import { createBrowserRouter } from 'react-router';
+
+export const router = createBrowserRouter([
+ {
+  path: "/",
+  Component: MainLayout,
+  children:
+  [
+    {
+      index: true,
+      element: <HomePage></HomePage>
+    }, 
+    {
+      path: "/books",
+      element: <Books></Books>
+    }
+  ]
+ }
+]);
+

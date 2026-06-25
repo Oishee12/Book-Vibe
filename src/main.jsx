b8b0f34aa498621a0bdec22644afe1c9a6ff4669
Oishe <yhsiokhan@gmail.com>
2026-06-25 11:42:47 +0600

    setup daisy ui and react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+const router = createBrowserRouter([
+  {
+path: "/",
+element: <div>hello world</div>
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h2 className='bg'>Hello this is my new react project</h2>
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )

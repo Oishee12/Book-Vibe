6c5599f5d7057b9590515c72e3a7cc42d274710f
Oishe <yhsiokhan@gmail.com>
2026-06-25 12:23:17 +0600

    Add navbar

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,24 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import MainLayout from './layout/MainLayout';
+import HomePage from './pages/homepage/HomePage';
+import Books from './pages/Books';
 
 const router = createBrowserRouter([
  {
   path: "/",
-  element: <MainLayout></MainLayout>
+  Component: MainLayout,
+  children:
+  [
+    {
+      index: true,
+      element: <HomePage></HomePage>
+    }, 
+    {
+      path: "/books",
+      element: <Books></Books>
+    }
+  ]
  }
 ]);
 
